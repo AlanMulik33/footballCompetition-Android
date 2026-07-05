37be1ce33b23f45254e98cffb2dfc486f00e1e9e
wisatwan <wisatwan06@gmail.com>
2026-07-05 13:42:15 +0800

    perbaiki masalah web

--- app/src/main/java/com/alzen/footballcompetition/MainActivity.java
+++ app/src/main/java/com/alzen/footballcompetition/MainActivity.java
@@ -2,7 +2,6 @@ package com.alzen.footballcompetition;
 
 import android.app.Activity;
 import android.os.Bundle;
-import android.view.View;
 import android.webkit.WebSettings;
 import android.webkit.WebView;
 import android.webkit.WebViewClient;
@@ -18,17 +17,14 @@ public class MainActivity extends Activity {
 
         webView = findViewById(R.id.webView);
 
-        // Matikan hardware rendering khusus WebView
-        webView.setLayerType(View.LAYER_TYPE_SOFTWARE, null);
-
         WebSettings settings = webView.getSettings();
         settings.setJavaScriptEnabled(true);
         settings.setDomStorageEnabled(true);
+        settings.setDatabaseEnabled(true);
         settings.setAllowFileAccess(true);
         settings.setAllowContentAccess(true);
         settings.setUseWideViewPort(true);
         settings.setLoadWithOverviewMode(true);
-        settings.setDatabaseEnabled(true);
 
         webView.setWebViewClient(new WebViewClient());
         webView.loadUrl("file:///android_asset/index.html");
